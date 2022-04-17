64245d7230970e0d83ad9f72244ca319caf9c315
dawon0304 <dawon0304@naver.com>
2022-04-17 23:25:19 +0900

    글내용추가 기능

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [글내용,글내용변경] = useState(['오늘은 React 공부를 했다.','어제는 새우랑 산책헀다.']);
+  let [입력값, 입력값변경] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <div>세줄 Diary</div>
+      </div>
+      {
+        글내용.map(function(a){
+          return(
+          <div className='list'>
+            <h3> {a} </h3>
+            <p>2022.04.17 작성</p>
+            <button className='removeBtn'>삭제</button>
+            <hr/>
+          </div>
+          )
+        })
+      }
+
+      <div className='form'>
+        <input onChange={ (e)=> {입력값변경(e.target.value)} }/>
+        <button onClick={()=> {
+          var arrayCopy = [...글내용]
+          arrayCopy.unshift(입력값);
+          글내용변경(arrayCopy);
+        } }>추가하기</button>
+      </div>
+      
     </div>
   );
 }
